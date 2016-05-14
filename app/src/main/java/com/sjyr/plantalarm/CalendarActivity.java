84a9872797b43624d93f12af1d5109b7c299f420
Junsuk Oh <a811219@gmail.com>
2016-05-14 12:10:23 +0900

    달력 라이브러리 2

--- app/src/main/java/com/sjyr/plantalarm/CalendarActivity.java
+++ app/src/main/java/com/sjyr/plantalarm/CalendarActivity.java
@@ -1,19 +1,34 @@
 package com.sjyr.plantalarm;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.squareup.timessquare.CalendarPickerView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class CalendarActivity extends AppCompatActivity {
+    private List<Date> mDates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        mDates = new ArrayList<>();
 
         // 올 해 1월
         Calendar thisYear = Calendar.getInstance();
@@ -23,7 +38,7 @@ public class CalendarActivity extends AppCompatActivity {
         Calendar nextYear = Calendar.getInstance();
         nextYear.add(Calendar.YEAR, 1);
 
-        CalendarPickerView calendar = (CalendarPickerView) findViewById(R.id.calendar_view);
+        final CalendarPickerView calendar = (CalendarPickerView) findViewById(R.id.calendar_view);
 
         // 오늘
         Date today = new Date();    // 현재 날짜 정보
@@ -35,7 +50,18 @@ public class CalendarActivity extends AppCompatActivity {
         calendar.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
             @Override
             public void onDateSelected(Date date) {
+                // 날짜 클릭
 
+                // 이미 들어있는지 확인
+                if (mDates.contains(date)) {
+                    mDates.remove(date);
+                } else {
+                    // 하이라이트
+                    mDates.add(date);
+                }
+
+                calendar.clearHighlightedDates();
+                calendar.highlightDates(mDates);
             }
 
             @Override
@@ -45,5 +71,75 @@ public class CalendarActivity extends AppCompatActivity {
         });
 //        calendar.init(today, nextYear.getTime())
 //                .inMode(RANGE);
+
+        try{
+            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/PlantAlarm", "calendar");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String str = br.readLine();
+            while(str != null){
+                Log.d("CalendarActivity", str);
+                Date date = new Date(Long.parseLong(str));
+                mDates.add(date);
+                str = br.readLine();
+            }
+
+            br.close();
+
+            // 색칠
+            calendar.highlightDates(mDates);
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+
+    // 화면이 안 보이기 직전 시점
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // 파일에 저장
+        if (isExternalStorageWritable()) {
+            File dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/PlantAlarm");
+            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/PlantAlarm", "calendar");
+
+            if (!dir.exists()) {
+                dir.mkdir();
+            } else {
+
+                try {
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    PrintWriter printWriter = new PrintWriter(fileOutputStream);
+
+                    for (int i = 0; i < mDates.size(); i++) {
+                        printWriter.println(mDates.get(i).getTime());
+                    }
+
+                    // 반영
+                    printWriter.flush();
+                    printWriter.close();
+                    fileOutputStream.close();
+
+                    Toast.makeText(CalendarActivity.this, "저장 완료", Toast.LENGTH_SHORT).show();
+
+                } catch (IOException e) {
+                    Toast.makeText(CalendarActivity.this, "저장이 실패 하였습니다", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            Toast.makeText(CalendarActivity.this, "저장이 실패 하였습니다", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
     }
 }
